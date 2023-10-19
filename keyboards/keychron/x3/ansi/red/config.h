4befeb82129ed0de2db08d051315096338fb9b7b
lalalademaxiya1 <2831039915@qq.com>
2023-10-19 11:56:59 +0800

    Update x3

--- keyboards/keychron/x3/ansi/red/config.h
+++ keyboards/keychron/x3/ansi/red/config.h
@@ -22,7 +22,7 @@
 #    define DRIVER_ADDR_1 0b1110100
 
 /* RGB Matrix Configuration */
-#    define LED_MATRIX_LED_COUNT 104
+#    define LED_MATRIX_LED_COUNT (104 + 12)
 
 /* Scan phase of led driver set as MSKPHASE_9CHANNEL(defined as 0x03 in CKLED2001.h) */
 #    define PHASE_CHANNEL MSKPHASE_9CHANNEL
